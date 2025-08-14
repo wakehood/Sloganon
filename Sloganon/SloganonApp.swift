79b7e2b75e24d59d09d7e91a3febdc35796f63a4
wakehood <wakehood@sonic.net>
2025-08-14 10:01:02 -0700

    Removing all original UIKit App and Realm DB in prepration for Swift UI and Version 2 Rewrite

---
+++ Sloganon/SloganonApp.swift
@@ -0,0 +1,16 @@
+//
+//  SloganonApp.swift
+//  Sloganon
+//
+//  Created by Sylvia Wake-Hood on 8/14/25.
+//
+  import SwiftUI
+
+@main
+struct MyExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
